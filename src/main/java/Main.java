afcfdd72a6d3d3416cf11890480c56021d45c13e
Dru <lehtin.aa@mail.ru>
2025-01-19 17:54:02 +0300

    try commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите скорость машины №1:");
+
         String name = scanner.next();
     }
 }
