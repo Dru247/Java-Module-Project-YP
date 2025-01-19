9d76fcd614c61fae6e2e95605e6d6ad4e0e68364
Dru <lehtin.aa@mail.ru>
2025-01-19 17:46:56 +0300

    add scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите скорость машины №1:");
+        String name = scanner.next();
     }
 }
